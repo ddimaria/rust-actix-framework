1414733fc90dc95be0aa6b95f56b87b035a321e6
David DiMaria <info@collectivesessions.com>
2019-10-14 09:51:23 -0600

    Login before running each integration test

--- src/tests/helpers.rs
+++ src/tests/helpers.rs
@@ -3,6 +3,7 @@ pub mod tests {
     use crate::auth::get_identity_service;
     use crate::config::CONFIG;
     use crate::database::{add_pool, init_pool, Pool};
+    use crate::handlers::auth::LoginRequest;
     use crate::routes::routes;
     use actix_web::dev::{Service, ServiceResponse};
     use actix_web::{test, web::Data, App};
@@ -11,6 +12,7 @@ pub mod tests {
 
     /// Helper for HTTP GET integration tests
     pub fn test_get(route: &str) -> ServiceResponse {
+        login();
         let mut app = test::init_service(
             App::new()
                 .wrap(get_identity_service())
@@ -23,6 +25,7 @@ pub mod tests {
 
     /// Helper for HTTP POST integration tests
     pub fn test_post<T: Serialize>(route: &str, params: T) -> ServiceResponse {
+        login();
         let mut app = test::init_service(
             App::new()
                 .wrap(get_identity_service())
@@ -58,5 +61,23 @@ pub mod tests {
     /// Returns a r2d2 Pooled Connection wrappedn in Actix Application Data
     pub fn get_data_pool() -> Data<Pool<MysqlConnection>> {
         Data::new(get_pool())
+    }
+
+    pub fn login() -> ServiceResponse {
+        let login_request = LoginRequest {
+            email: "admin@admin.com".into(),
+            password: "123".into(),
+        };
+        let mut app = test::init_service(
+            App::new()
+                .wrap(get_identity_service())
+                .configure(add_pool)
+                .configure(routes),
+        );
+        let request = test::TestRequest::post()
+            .set_json(&login_request)
+            .uri("/api/v1/login")
+            .to_request();
+        test::block_on(app.call(request)).unwrap()
     }
 }
