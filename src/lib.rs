855b5c4c4be6df4e9961803454e4f227edb35c1d
David DiMaria <info@collectivesessions.com>
2020-02-18 08:59:16 -0700

    Convert to a library and add an example implementation

--- src/lib.rs
+++ src/lib.rs
@@ -11,22 +11,22 @@ extern crate validator_derive;
 
 use crate::server::server;
 
-mod auth;
-mod cache;
-mod config;
-mod database;
-mod errors;
-mod extractors;
+pub mod auth;
+pub mod cache;
+pub mod config;
+pub mod database;
+pub mod errors;
+pub mod extractors;
 pub mod handlers;
-mod helpers;
-mod middleware;
-mod models;
-mod routes;
-mod schema;
-mod server;
-mod state;
-mod tests;
-mod validate;
+pub mod helpers;
+pub mod middleware;
+pub mod models;
+pub mod routes;
+pub mod schema;
+pub mod server;
+pub mod state;
+pub mod tests;
+pub mod validate;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
